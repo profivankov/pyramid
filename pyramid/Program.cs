138dea71a6f43e202a7d2b4c53af99957d7577c4
BonBon <BonBon@DESKTOP-15RAB9Q>
2020-07-02 11:01:49 +0300

    Add project files.

---
+++ pyramid/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Enumerable.Range(2, height).Aggregate("*", (s, i) => s + Environment.NewLine + new string('*', i)));
+        }
+    }
+}
